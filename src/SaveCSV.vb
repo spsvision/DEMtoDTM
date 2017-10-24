56224689e0c94fe5bd665e31dca56d56521ce336
SPS Vision <steven-ps@hotmail.com>
2017-10-23 19:09:10 -0700

    Update SaveCSV.vb

--- src/SaveCSV.vb
+++ src/SaveCSV.vb
@@ -1,7 +1,7 @@
 Sub test()
 
 Application.DisplayAlerts = False
-
+  strFullname = "coordinates"
 ThisWorkbook.Sheets(strSourceSheet).Copy
 ActiveWorkbook.SaveAs Filename:=strFullname, FileFormat:=xlCSV, CreateBackup:=True
 ActiveWorkbook.Close
