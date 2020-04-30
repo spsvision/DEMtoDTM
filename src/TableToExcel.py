12da2c4b1154b803d93d9f38fff96d5b93fe6036
Steven Samuel <steven-ps@hotmail.com>
2020-04-29 17:52:06 -0700

    Update TableToExcel.py

--- src/TableToExcel.py
+++ src/TableToExcel.py
@@ -2,7 +2,7 @@ import arcpy
 arcpy.env.workspace = "c:/data/output"
 
 # Convert directly to an Excel workbook. ArcMap can only export 65535 rows though.
-arcpy.TableToExcel_conversion("100x", "C:/Users/Guest Admin/Desktop/table.xlx")
+arcpy.TableToExcel_conversion("100x", "C:/Users/User1/Desktop/table.xlx")
 
 # Convert to a 
 arcpy.TableToDBASE_conversion(["TABLE"], "C:/output")
